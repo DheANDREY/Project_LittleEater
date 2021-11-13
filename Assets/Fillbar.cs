0371c01477fe48e9180481131f274d21e2780410
AndreyD <56178406+DheANDREY@users.noreply.github.com>
2021-11-13 12:21:21 +0700

    MoveDash+Bar

---
+++ Assets/Fillbar.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine; 
+using UnityEngine.UI;
+
+public class Fillbar : MonoBehaviour
+{
+	public Slider slider1;
+    public int maxDash = 100;
+    public int currentDash;
+
+    private WaitForSeconds regenTime = new WaitForSeconds(0.1f);
+    private Coroutine regenerate;
+
+    public static Fillbar instance;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    private void Start()
+    {
+        currentDash = maxDash;  
+        slider1.maxValue = maxDash;
+        slider1.value = maxDash;
+    }
+
+    public void kurang(int nilai)
+    {
+        if(currentDash - nilai >= 0)
+        {
+            currentDash -= nilai;
+            Debug.Log(currentDash);
+            slider1.value = currentDash;      
+            if(regenerate != null)
+            {
+                StopCoroutine(regenerate);
+            }
+            regenerate = StartCoroutine(regenDash());
+        }
+        else
+        {
+            Debug.Log("Tidak cukup");
+        }
+    }
+    private IEnumerator regenDash()
+    {
+        yield return new WaitForSeconds(5);
+        while(currentDash < maxDash)
+        {
+            currentDash += maxDash/50 ;
+            slider1.value = currentDash;
+            yield return regenTime;
+        }
+
+    }
+}
---
+++ Assets/spawn.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class spawn : MonoBehaviour
+{
+    public GameObject Bar1;
+    public bool spawnBar;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (spawnBar)
+        {
+            spawnBar = false;
+            munculBar();
+  
+
+        }
+    }
+    private void munculBar(int x = -30)
+    {
+        //for (int i = 1; i < 3; i++)
+        //{
+            GameObject g = Instantiate(Bar1, new Vector3(x, 0, 0), Quaternion.identity) as GameObject;
+            g.transform.SetParent(GameObject.FindGameObjectWithTag("bar").transform, false);
+       // }
+    }
+}
