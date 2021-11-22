5943babfc259a09a25ac5cead4a8e04e30a6a55f
AndreyD <56178406+DheANDREY@users.noreply.github.com>
2021-11-22 11:53:56 +0700

    Test-arah(msh gagal)

--- Assets/Scripts/Move_CharUtama.cs
+++ Assets/Scripts/Move_CharUtama.cs
@@ -1,5 +1,4 @@
-using System.Collections;
-using System.Collections.Generic;
+using DragonBones;
 using UnityEngine;
 
 public class Move_CharUtama : MonoBehaviour
@@ -12,22 +11,26 @@ public class Move_CharUtama : MonoBehaviour
 
     private void Awake()
     {
+        UnityArmatureComponent armatureComponent = GetComponent<UnityArmatureComponent>();
         _rigidBody = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
+
     }
 
+    private UnityArmatureComponent _armature;
     public Fillbar fb;
     private void Start()
     {
         fb = GetComponent<Fillbar>();
-        
+        _armature = GetComponent<UnityArmatureComponent>();
+
     }
     // Update is called once per frame
     void Update()
     {
         Vector3 move = new Vector3(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
         Vector3 velocity = _rigidBody.velocity;
-        if(move != Vector3.zero)
+        if (move != Vector3.zero)
         {
             velocity += move * _acceleration * Time.deltaTime;
             velocity.x = Mathf.Clamp(velocity.x, -_maxSpeed, _maxSpeed);
@@ -37,9 +40,19 @@ public class Move_CharUtama : MonoBehaviour
         {
             velocity.x += (velocity.x > 0 ? -1 : 1) * _acceleration * Time.deltaTime;// Mathf.Clamp(velocity.x, -_maxSpeed, _maxSpeed);
             velocity.y = (velocity.y > 0 ? -1 : 1) * _acceleration * Time.deltaTime;// Mathf.Clamp(velocity.y, -_maxSpeed, _maxSpeed);
-        }        
+        }
         _rigidBody.velocity = velocity;
         dash();
+
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            _armature.animation.Play("move", 1);
+            return;
+        }
+        else if (Input.GetKeyDown(KeyCode.D))
+        {
+            _armature.animation.Play("move", 1);
+        }
     }
     private bool boostUsed = false;
     private float spdDash = 100f;
@@ -48,7 +61,7 @@ public class Move_CharUtama : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.Space) && Fillbar.instance.currentDash >= 50)
         {
-     
+
             Fillbar.instance.kurang(50);
             _rigidBody.AddForce(_rigidBody.velocity * spdDash);
             boostUsed = true;
