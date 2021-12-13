4c17688bd812e2d07db7fa4e15223f3b211dc94d
Mau'idzoh Hasanah <1910631170095@student.unsika.ac.id>
2021-12-13 20:20:48 +0700

    Main Menu Screen

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void selectScene()
+    {
+        switch (this.gameObject.name)
+        {
+            case "PlayButton":
+                SceneManager.LoadScene("CharacterMove");
+                break;
+            case "SettingsButton":
+                SceneManager.LoadScene("Settings");
+                break;
+            case "ExitButton":
+                Application.Quit();
+                break;
+            case "CreditsButton":
+                SceneManager.LoadScene("Credits");
+                break;
+        }
+    }
+}
