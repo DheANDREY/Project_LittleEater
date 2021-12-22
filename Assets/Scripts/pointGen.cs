8b5cb9de39ae01cba1ee3c6095a1facd7e6b0126
AndreyD <56178406+DheANDREY@users.noreply.github.com>
2021-12-22 14:04:18 +0700

    Shop tahap awal

--- Assets/Scripts/pointGen.cs
+++ Assets/Scripts/pointGen.cs
@@ -6,6 +6,7 @@ using UnityEngine.UI;
 public class pointGen : MonoBehaviour
 {
     public Text poin;
+    public Text poin1;
     public Move_CharUtama mcu;
 
     // Start is called before the first frame update
@@ -17,5 +18,6 @@ public class pointGen : MonoBehaviour
     void Update()
     {
         poin.text = mcu.Score.ToString();
+        poin1.text = mcu.Score.ToString();
     }
 }
