ec7e713997deba32904232d601e35451afaae621
Mau'idzoh Hasanah <1910631170095@student.unsika.ac.id>
2021-12-23 12:06:40 +0700

    encyclopedia screen

---
+++ Assets/Scripts/CloseToLobby.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CloseToLobby : MonoBehaviour
+{
+    public void selectScene()
+    {
+        switch (this.gameObject.name)
+        {
+            case "CloseButton":
+                SceneManager.LoadScene("LobbyLevel1");
+                break;
+        }
+    }
+}
