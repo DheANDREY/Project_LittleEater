b182c3405bc2d95fb3c86aaa008835359cd0c1a0
Mau'idzoh Hasanah <1910631170095@student.unsika.ac.id>
2021-12-23 19:59:46 +0700

    Tutorial 1

---
+++ Assets/Scripts/PopUp.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PopUp : MonoBehaviour
+{
+    public GameObject PopUp1;
+    public GameObject PopUp2;
+
+    //public void Start()
+    //{
+      //  PopUp1.SetActive(true);
+        //PopUp2.SetActive(false);
+    //}
+
+    public void Update()
+    {
+        if (Input.GetKeyDown("space"))
+        {
+            PopUp1.SetActive(false);
+            PopUp2.SetActive(true);
+        }
+    }
+
+    /*public void OnMouseDown()
+    {
+        PopUp1.SetActive(false);
+        PopUp2.SetActive(true);
+    }*/
+}
