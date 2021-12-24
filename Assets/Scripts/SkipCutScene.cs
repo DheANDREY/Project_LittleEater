18ffc0873ecfda2057b7a7e843f418abdb84743b
Mau'idzoh Hasanah <1910631170095@student.unsika.ac.id>
2021-12-24 20:06:21 +0700

    Memperbaiki UI tiap screen dan menambah skip pada cutscene

---
+++ Assets/Scripts/SkipCutScene.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SkipCutScene : MonoBehaviour
+{
+    public void selectScene()
+    {
+        switch (this.gameObject.name)
+        {
+            case "Skip":
+                SceneManager.LoadScene("Tutorial1");
+                break;
+        }
+    }
+}
