215d58ccc7118958ea3666feb2d893d44adef68c
Mau'idzoh Hasanah <1910631170095@student.unsika.ac.id>
2022-01-04 12:29:22 +0700

    Revert "Revert "settings ga muncul lagi 2 kali tapi mulai dari awal lagi pas pindah scene""
    This reverts commit 0f5ccdfe4a543e568e84646bb3645c4b60d253b9.

--- Assets/Scripts/Lobby1.cs
+++ Assets/Scripts/Lobby1.cs
@@ -17,6 +17,10 @@ public class Lobby1 : MonoBehaviour
                 SceneManager.LoadScene("Settings");
                 break;
             case "Back":
+                if (FindObjectOfType<MainMenuBGM>().gameObject != null)
+                {
+                    DestroyImmediate(FindObjectOfType<MainMenuBGM>().gameObject);
+                }
                 SceneManager.LoadScene("MainMenu");
                 break;
             case "Next":
